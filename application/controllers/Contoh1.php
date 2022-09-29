9a20b19871ab148b498c2cff239f42a1cbf4476a
Re354 <rizal354r@gmail.com>
2022-09-29 08:24:47 +0700

    initial commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,15 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Rizal
+            Saya tinggal di daerah Dengklok <br>
+            olah raga yang saya sukai adalah
+            Bulutangkis";
+    }
+}
+
+
+?>
